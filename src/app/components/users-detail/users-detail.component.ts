4f2215d3ad259e2bba30f42714c2da6780b4a7f6
Emerson Vergara <ee-web@hotmail.com>
2022-08-26 14:39:51 -0500

    Se agrega  mapa

--- src/app/components/users-detail/users-detail.component.ts
+++ src/app/components/users-detail/users-detail.component.ts
@@ -1,8 +1,13 @@
+import { CrossOrigin } from './../../../../node_modules/@types/leaflet/index.d';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { UserService } from '../../services/user.service';
+
+import * as Leaflet from 'leaflet';
+
 import { ResponseAPI } from 'src/app/model/response';
 import { User } from '../../model/user';
+import { Coordinates } from '../../model/coordinates';
 
 @Component({
   selector: 'app-users-detail',
@@ -15,6 +20,17 @@ export class UsersDetailComponent implements OnInit {
   btnSaveText: 'Save Changes' | 'Edit' = 'Edit';
   response: ResponseAPI;
   user: User;
+  marker = new Leaflet.Icon({
+    iconUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/images/marker-icon.png',
+    iconSize: [32, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/images/marker-shadow.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [41, 41],
+  });
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,6 +43,7 @@ export class UsersDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.data();
+    this.loadMap(this.user.coordinates);
   }
 
   data(): void {
@@ -34,7 +51,7 @@ export class UsersDetailComponent implements OnInit {
       this.activatedRoute.snapshot.data['resolvedUseResponse'].results[0]
     );
   }
-/*
+  /*
   viewRouter(): void {
     this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
       this.uuid = '' + params.get('uuid');
@@ -56,5 +73,34 @@ export class UsersDetailComponent implements OnInit {
       //Logic to update the user on the back end
       console.log('updating...');
     }
+  }
+
+  private loadMap(coordinates: Coordinates): void {
+    var map = Leaflet.map('map', {
+      center: [Number(coordinates.latitude), Number(coordinates.longitude)],
+      zoom: 10,
+    });
+    const mainLayer = Leaflet.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        maxZoom: 30,
+        crossOrigin: true,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }
+    );
+
+    mainLayer.addTo(map);
+    const marker = Leaflet.marker(
+      [Number(coordinates.latitude), Number(coordinates.longitude)],
+      { icon: this.marker }
+    );
+    marker
+      .addTo(map)
+      .bindPopup(`${this.user.firstName} 's Location`)
+      .openPopup();
   }
 }
