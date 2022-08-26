6bcf7dee7d5c53c3be0e2020b31e105938f1a674
Emerson Vergara <ee-web@hotmail.com>
2022-08-25 22:48:18 -0500

    Configuramos rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { UsersDetailComponent } from './components/users-detail/users-detail.component';
 
-const routes: Routes = [];
+let ROUTER_INIT: string = 'users';
+
+const routes: Routes = [
+  {
+    path: ROUTER_INIT,
+    component: UsersComponent,
+  },
+  {
+    path: 'user/:uuid',
+    component: UsersDetailComponent,
+  },
+  {
+    path: '',
+    redirectTo: ROUTER_INIT,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: ROUTER_INIT,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
